97e5011f38afc4cfcb046c2786c860883232654b
Stins <danielpstinson@gmail.com>
2024-08-18 09:18:24 -0500

    Enabled limited Zeus mission_params.hpp

--- Missionframework/ui/mission_params.hpp
+++ Missionframework/ui/mission_params.hpp
@@ -333,7 +333,7 @@ class Params {
         title = $STR_PARAM_LIMITEDZEUS;
         values[] = {1, 0};
         texts[] = {$STR_PARAMS_ENABLED, $STR_PARAMS_DISABLED};
-        default = 0;
+        default = 1;
     };
     class ZeusAddEnemies {
         title = $STR_PARAM_ZEUSADDENEMIES;
